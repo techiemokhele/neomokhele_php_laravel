dd795e5851ca8c679d031ac16a73cfd1061db903
techiemokhele <neomokhele23@gmail.com>
2024-08-11 10:44:43 +0200

    Replace hardcoded job data with Job model instances in web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,46 +9,12 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => 'R50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => 'R60,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => 'R26,000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => 'R50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => 'R60,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => 'R26,000'
-        ]
-    ];
-
-    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
