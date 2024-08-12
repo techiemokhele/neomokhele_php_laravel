d87218611f8963396b60c56531b9e56c5e12f134
techiemokhele <neomokhele23@gmail.com>
2024-08-12 07:21:06 +0200

    Refactor Job model to extend Eloquent\Model, replace static methods with database table mapping

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => 'R50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => 'R60,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => 'R26,000'
-            ]
-        ];
-    }
+    protected $table = 'jobs_listing';
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
